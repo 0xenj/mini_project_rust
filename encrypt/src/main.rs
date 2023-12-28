cc19e5ff7cc98b1c1d7a86b5f50a8657dbddbde8
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-28 11:22:27 +0100

    improve main.rs

--- encrypt/src/main.rs
+++ encrypt/src/main.rs
@@ -6,32 +6,33 @@ use std::io::{ Read, Write };
 use std::env;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  if args.len() < 2 {
-    println!("Provide a file to convert.\nUsage: {} filename", args[0]);
-  } else {
-    let ref filename = args[1];
+    if args.len() < 3 {
+        println!("Provide a file and mode.\nUsage: {} filename [true|false]", args[0]);
+        return;
+    }
+
+    let filename = &args[1];
+    let mode = args[2].parse::<bool>().unwrap_or(true);
+
     match File::open(filename) {
-      Ok(mut file) => {
-        let mut content = String::new();
-        file.read_to_string(&mut content).unwrap();
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
 
-        let converted = sha9(&content);
+            let converted = sha9(&content, mode);
 
-        // create opens file as write-only, deletes existing content
-        match File::create(filename) {
-          Err(e) => panic!("Couldn't create {}: {}", filename, e),
-          Ok(mut file) => {
-            match file.write_all(converted.as_bytes()) {
-              Err(e) => panic!("Couldn't write to {}: {}", filename, e),
-              Ok(_) => println!("Converted {}!", filename)
+            match File::create(filename) {
+                Err(e) => panic!("Couldn't create {}: {}", filename, e),
+                Ok(mut file) => {
+                    match file.write_all(converted.as_bytes()) {
+                        Err(e) => panic!("Couldn't write to {}: {}", filename, e),
+                        Ok(_) => println!("Processed {}!", filename)
+                    };
+                }
             };
-          }
-        };
-      },
-      Err(e) => println!("Error opening file {}: {}", filename, e)
+        },
+        Err(e) => println!("Error opening file {}: {}", filename, e)
     };
-  }
-
 }
